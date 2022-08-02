79ee3792c14c674b9c81138b1eee8a57c49d38e7
Iri-Sha <a72999555@gmail.com>
2022-08-03 01:34:17 +0500

    error correction config1

--- constants/config.js
+++ constants/config.js
@@ -1,5 +1,4 @@
-const SECRET_KEY = 'some-secret-key';
-const MONGO_DEFAULT_URL = 'mongodb://localhost:27017/bitfilmsdb';
+module.exports.SECRET_KEY = 'some-secret-key';
+module.exports.MONGO_DEFAULT_URL = 'mongodb://localhost:27017/bitfilmsdb';
 
-const linkRegExp = /^https?:\/\/(w{3}\.)?[a-zA-Z0-9-.]+\.[a-zA-Z]{2,}([a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=]+)*#*$/;
-module.exports = { linkRegExp, SECRET_KEY, MONGO_DEFAULT_URL };
+module.exports.linkRegExp = /^https?:\/\/(w{3}\.)?[a-zA-Z0-9-.]+\.[a-zA-Z]{2,}([a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=]+)*#*$/;
