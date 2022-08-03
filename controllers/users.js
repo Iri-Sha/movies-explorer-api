03f34d23b108b3e9595b78b2b85fa59342d0e716
Iri-Sha <a72999555@gmail.com>
2022-08-03 20:07:40 +0500

    feature: fix user controllers

--- controllers/users.js
+++ controllers/users.js
@@ -40,6 +40,8 @@ module.exports.updateProfile = (req, res, next) => {
     .catch((err) => {
       if (err.name === 'ValidationError') {
         next(new BadRequestError('Переданы некорректные данные при обновлении профиля.'));
+      } else if (err.code === 11000) {
+        next(new ConflictError('Указанный email уже зарегистрирован'));
       } else {
         next(err);
       }
